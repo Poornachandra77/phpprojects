c2a72ed64a9781b33ff4f523e7c40eb35d9032d9
Poornachandra77 <ckongarac@gmail.com>
2020-12-21 18:04:08 +0530

    first upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$sname = "localhost"; 
+$uname = "phpmyadmin";
+$dbname = "dataentry";
+$password = "CHANDU";
+
+$conn = mysqli_connect($sname, $uname, $password,$dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	
+    <link rel="stylesheet" type="text/css" href="loginstyle.css">
+</head>
+<body>
+    <div class="form"><br>
+    <form  action="login.php" method="post">
+            <center><strong> Login </strong><br><br></center>
+            <input type="email" name="uname" class="email" placeholder="Username">
+            <input type="password" name="password" class="password" placeholder="Password"><br>
+            <input type="submit" name="submit" class="submit" value="Login" name="submit" >
+            <div class="newuser">
+            If not Registered?  <a href="register.php">Register</a>
+            </div>
+    </form>
+    </div>  
+
+</body>
+</html>
+
+
